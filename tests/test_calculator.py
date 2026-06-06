1a57163f982d776fbdf5100f82f54b48b508b365
Yuki <fantasyyuki01@gmail.com>
2026-06-06 11:59:48 +0900

    test: revert intentional failure

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -2,7 +2,7 @@ import pytest
 from src.calculator import add, subtract, multiply, divide
 
 def test_add():
-    assert add(2, 3) == 99  # わざと間違える
+    assert add(2, 3) == 5   
 
 def test_subtract():
     assert subtract(5, 3) == 2
